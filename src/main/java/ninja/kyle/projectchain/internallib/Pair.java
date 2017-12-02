b802c8e2b4265d28a45b098644a62a1dd1a4006c
Kyle-James Condon <contact@kyle.ninja>
2017-12-02 13:53:53 +0000

    Created pair utility class

---
+++ src/main/java/ninja/kyle/projectchain/internallib/Pair.java
@@ -0,0 +1,39 @@
+package ninja.kyle.projectchain.internallib;
+
+import java.util.Objects;
+
+public class Pair<S, T> {
+
+  private final S left;
+  private final T right;
+
+  public Pair(S left, T right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public S getLeft() {
+    return left;
+  }
+
+  public T getRight() {
+    return right;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(left, right);
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (! (object instanceof Pair)) {
+      return false;
+    }
+
+    Pair p = (Pair) object;
+
+    return getLeft().equals(p.getLeft()) && getRight().equals(p.getRight());
+  }
+
+}
