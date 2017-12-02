259f406474aae898628b1e543f8fe1e49e59b1f8
Kyle-James Condon <kyle-james.condon16@ic.ac.uk>
2017-12-02 16:49:45 +0000

    Simplified PriceTickerSet.

--- src/main/java/ninja/kyle/projectchain/PriceTickerSet.java
+++ src/main/java/ninja/kyle/projectchain/PriceTickerSet.java
@@ -9,21 +9,12 @@ import ninja.kyle.projectchain.internallib.Pair;
 
 public class PriceTickerSet {
 
-  private final ImmutableSet<Pair<Asset, Asset>> tradingPairs;
   private final ImmutableMap<Pair<Asset, Asset>, PriceHistory> priceHistoryMap;
 
   public PriceTickerSet(ImmutableSet<Pair<Asset, Asset>> tradingPairs) {
-    this.tradingPairs = tradingPairs;
-    
     ImmutableMap.Builder<Pair<Asset, Asset>, PriceHistory> builder = ImmutableMap.builder();
-    ImmutableSet<Pair<Asset, Asset>> pairs = getTradingPairs();
-    pairs.forEach(p -> builder.put(p, new PriceHistory()));
-    
+    tradingPairs.forEach(p -> builder.put(p, new PriceHistory()));
     priceHistoryMap = builder.build();
-  }
-
-  public final ImmutableSet<Pair<Asset, Asset>> getTradingPairs() {
-    return tradingPairs;
   }
 
   public final Optional<Double> getLastPrice(Pair<Asset, Asset> tradingPair) {
